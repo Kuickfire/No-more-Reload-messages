06c4b097e0675f7bd30f98237d8f30d425ca3e5e
Kuickfire <ricardissimo97@gmail.com>
2025-04-17 14:54:08 +0100

    Update No more Reload messages.lua
    added the same ability to full ammo vveapons

--- No more Reload messages/scripts/mods/No more Reload messages/No more Reload messages.lua
+++ No more Reload messages/scripts/mods/No more Reload messages/No more Reload messages.lua
@@ -9,10 +9,12 @@ InteractionDefinitions.pickup_object.client.can_interact = function(interactor_u
 
   if pickup_settings and pickup_settings.type == "ammo" then
     if inventory_extension:has_ammo_consuming_weapon_equipped("throwing_javelin") or
-       inventory_extension:has_ammo_consuming_weapon_equipped("throwing_axe") and pickup_settings.pickup_name == "all_ammo" then
+       inventory_extension:has_ammo_consuming_weapon_equipped("throwing_axe") and pickup_settings.pickup_name == "all_ammo" or 
+       inventory_extension: has_full_ammo() then
       return false, nil
     end
   end
+  
 
   return original_function(interactor_unit, interactable_unit, ...)
 end
